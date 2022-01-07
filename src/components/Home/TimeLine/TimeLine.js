e542cef0a743092c42468e17557400b53e89cfee
jatinmakad <38394914+jatinmakad@users.noreply.github.com>
2022-01-07 20:30:00 +0530

    let

--- src/components/Home/TimeLine/TimeLine.js
+++ src/components/Home/TimeLine/TimeLine.js
@@ -5,6 +5,7 @@ import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
 import TimelineConnector from "@material-ui/lab/TimelineConnector";
 import TimelineContent from "@material-ui/lab/TimelineContent";
 import TimelineDot from "@material-ui/lab/TimelineDot";
+import Line from "../../../images/line.png";
 const TimeLine = () => {
   return (
     <div
@@ -13,9 +14,31 @@ const TimeLine = () => {
         display: "flex",
         justifyContent: "flex-start",
         alignItems: "flex-start",
+        marginBottom:"5px"
       }}
     >
-      <Timeline style={{ padding: "0" }} className="timeline">
+      <div style={{
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          height: "100%"
+      }}>
+        <img src={Line} alt="" style={{marginRight:"10px"}} />
+        <div style={{
+           display: "flex",
+           flexDirection:"column",
+        }}>
+          <div style={{ fontWeight: "700",marginBottom:"15px", fontSize: "13px" }}>
+            Ceo at microsoft ( 2016 - 2018)
+            <p style={{ fontSize: "12px",fontWeight:"lighter" }}>Working on different dom...</p>
+          </div>
+          <div style={{ fontWeight: "700", fontSize: "13px" }}>
+          Director- at Microsoft
+            <p style={{ fontSize: "12px",fontWeight:"lighter"  }}>Workedd</p>
+          </div>
+        </div>
+      </div>
+      {/* <Timeline style={{ padding: "0" }} className="timeline">
         <TimelineItem>
           <TimelineSeparator>
             <TimelineConnector />
@@ -32,7 +55,6 @@ const TimeLine = () => {
         <TimelineItem>
           <TimelineSeparator>
             <TimelineDot style={{ background: "#FF7070" }} />
-            {/* <TimelineConnector /> */}
           </TimelineSeparator>
           <TimelineContent>
             <p style={{ fontWeight: "800", fontSize: "13px" }}>
@@ -42,7 +64,7 @@ const TimeLine = () => {
             <p style={{ fontSize: "12px" }}>Workedd</p>
           </TimelineContent>
         </TimelineItem>
-      </Timeline>
+      </Timeline> */}
     </div>
   );
 };
